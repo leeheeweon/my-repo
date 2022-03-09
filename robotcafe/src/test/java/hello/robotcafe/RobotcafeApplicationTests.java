56e2346d4c21610fa25f25a4f9ff76b066c6f0a2
leeheeweon <ggoddii@naver.com>
2022-03-09 11:47:22 +0900

    create project

---
+++ robotcafe/src/test/java/hello/robotcafe/RobotcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.robotcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
